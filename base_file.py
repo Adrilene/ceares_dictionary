6e49f39c5390a68a280c9e1410a158c8f5b30123
adrilene <adrilenefonseca@gmail.com>
2020-10-08 17:19:24 -0300

    generating random words

--- base_file.py
+++ base_file.py
@@ -1,6 +1,8 @@
+import random
+
 file = open('list_words.txt', 'r')
 
 lines = file.readlines()
  
-for line in lines:
-    print('the meaning is...')
+print('Generating random word... ')
+print(lines[random.randint(0, len(lines)-1)])
