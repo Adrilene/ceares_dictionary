23efedee56a2c12f49fda89307c5fb5ffb73b866
Denis Menezes de Sousa <denismzsousa@gmail.com>
2020-12-16 15:17:44 -0300

    config to heroku

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
-#from base_file import words
+
+# from base_file import words
 from bs4 import BeautifulSoup as bs
 import random
 import requests
@@ -7,22 +8,26 @@ import requests
 
 app = Flask(__name__)
 
-def transform_word(word):
-    return str(word).replace('</h2>', '').split('. ')[1]
 
-@app.route('/', methods=['GET'])
+def transform_word(word):
+    return str(word).replace("</h2>", "").split(". ")[1]
+
+
+@app.route("/", methods=["GET"])
 def index():
     page = requests.get("https://www.dicionariopopular.com/dicionario-cearense-girias/")
-    soup = bs(page.content, 'html.parser')
-    words = list(soup.find_all('h2'))
+    soup = bs(page.content, "html.parser")
+    words = list(soup.find_all("h2"))
     words.pop()
     words = [transform_word(word) for word in words]
     word = random.choice(words)
-    return render_template('index.html', word=word)
+    return render_template("index.html", word=word)
 
-@app.route('/support', methods=['GET'])
+
+@app.route("/support", methods=["GET"])
 def index():
     return "Woeking (Ceares Dictionary)"
 
-if '__main__' ==__name__:
+
+if "__main__" == __name__:
     app.run()
