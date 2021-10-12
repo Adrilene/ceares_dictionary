c7936130505927d079b282cc23a194b2c60ca274
Denis <denis.sousa@aluno.uece.br>
2021-10-12 16:56:47 -0300

    add definition of words

--- wsgi.py
+++ wsgi.py
@@ -12,6 +12,9 @@ app = Flask(__name__)
 def transform_word(word):
     return str(word).replace("</h2>", "").split(". ")[1]
 
+def transform_definition(definition):
+    return str(definition).replace('<p class="text-justify">', "").replace('</p>', "").strip()
+
 
 @app.route("/", methods=["GET"])
 def index():
@@ -21,7 +24,15 @@ def index():
     words.pop()
     words = [transform_word(word) for word in words]
     word = random.choice(words)
-    return render_template("index.html", word=word)
+
+    page = requests.get(f"https://www.dicionarioinformal.com.br/{word}")
+    soup = bs(page.content, "html.parser")
+    try:
+        definition = transform_definition(soup.find_all("p", {"class": "text-justify"})[0])
+    except:
+        definition = "Did not find definition"
+    # print(f'WORD:{word} \n DEFINITION:{definition}')
+    return render_template("index.html", word=word, definition=definition)
 
 
 @app.route("/health_check", methods=["GET"])
